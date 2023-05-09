23536da3a0fc45097c8d4c2ba87fd29353d25138
Poliuszko <poliuszko@gmail.com>
2023-05-09 20:30:58 +0200

    Button asking about number of squares

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
 "use strict";
-const r = 10;
-const c = 10;
 
-function changeColor(e) {
-  console.log(mouseDown, e.type);
-  console.log(e.type === "mouseover" && mouseDown);
-  if (e.type === "mouseover" && mouseDown) {
-    this.classList.add("hoverc");
-  }
-}
+const btn_set_sq = document.getElementById("set_sq");
+console.log(btn_set_sq);
 
-let mouseDown = false;
-document.onmousedown = () => (mouseDown = true);
-document.onmouseup = () => (mouseDown = false);
+btn_set_sq.addEventListener("click", function () {
+  document.getElementById("container").innerHTML = "";
+  const r = prompt("How many squares");
+  console.log("r", r);
+  generate_matrix(r);
+});
 
-for (let indexr = 0; indexr < r; indexr++) {
-  var div_row = document.createElement("div");
-  div_row.style = "display: flex; align-content: space-around";
-  div_row.style.width = "100%";
-  div_row.className = "divId";
-
-  for (let indexc = 0; indexc < c; indexc++) {
-    var div_col = document.createElement("div");
-    div_col.style.width = "auto";
-    div_col.style.padding = "5px";
-    div_col.style.height = "auto";
-
-    //div_col.style.background = "gray";
-    div_col.style.color = "white";
-    div_col.innerHTML = `${indexr}, ${indexc}`;
-    div_col.className = "divIdCol";
-
-    div_col.style.border = "1px solid black";
-    div_col.style.borderRadius = "2px ";
-    div_col.display = "flex";
-    div_col.addEventListener("mouseover", changeColor);
-    div_col.addEventListener("mousedown", function (e) {
+function generate_matrix(r) {
+  function changeColor(e) {
+    console.log(mouseDown, e.type);
+    console.log(e.type === "mouseover" && mouseDown);
+    if (e.type === "mouseover" && mouseDown) {
       this.classList.add("hoverc");
-    });
-
-    //e.target.classList.add("hover");
-    //e.target.style.backgroundColor = "red";
-
-    //div_col.addEventListener('mousedown', self.classList.toggle('hover'))
-
-    div_row.appendChild(div_col);
-    //console.log(indexr, indexc);
+    }
   }
-  document.getElementById("container").appendChild(div_row);
-}
 
+  let mouseDown = false;
+  document.onmousedown = () => (mouseDown = true);
+  document.onmouseup = () => (mouseDown = false);
+
+  for (let indexr = 0; indexr < r; indexr++) {
+    var div_row = document.createElement("div");
+    div_row.style = "display: flex; align-content: space-around;";
+    div_row.style.width = "100%";
+    div_row.className = "divId";
+
+    for (let indexc = 0; indexc < r; indexc++) {
+      var div_col = document.createElement("div");
+      div_col.style.width = "auto";
+      div_col.style.padding = "5px";
+      div_col.style.height = "auto";
+
+      //div_col.style.background = "gray";
+      div_col.style.color = "white";
+      div_col.innerHTML = `${indexr}, ${indexc}`;
+      div_col.className = "divIdCol";
+
+      div_col.style.border = "1px solid black";
+      div_col.style.borderRadius = "2px ";
+      div_col.display = "flex";
+      div_col.addEventListener("mouseover", changeColor);
+      div_col.addEventListener("mousedown", function (e) {
+        this.classList.add("hoverc");
+      });
+
+      //e.target.classList.add("hover");
+      //e.target.style.backgroundColor = "red";
+
+      //div_col.addEventListener('mousedown', self.classList.toggle('hover'))
+
+      div_row.appendChild(div_col);
+      //console.log(indexr, indexc);
+    }
+    document.getElementById("container").appendChild(div_row);
+  }
+}
 //gridElement.addEventListener('mouseover', changeColor)
 //    gridElement.addEventListener('mousedown', changeColor)
